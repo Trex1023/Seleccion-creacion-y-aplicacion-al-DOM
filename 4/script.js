905ab5418b8aff7189126f66b4bf668d3038b290
Trex1023 <121815633+Trex1023@users.noreply.github.com>
2026-08-19 19:32:25 +0000

    4

--- 4/script.js
+++ 4/script.js
@@ -1,3 +1,12 @@
 const div = document.createElement('div');
 div.id = "container"
-div.appendChild()
+const text1 = document.createElement('p')
+text1.textContent = "hola"
+const text2 = document.createElement('p')
+text2.textContent = "mundo"
+const text3 = document.createElement('p')
+text3.textContent = ":)"
+div.appendChild(text1)
+div.appendChild(text2)
+div.appendChild(text3)
+document.body.appendChild(div);
