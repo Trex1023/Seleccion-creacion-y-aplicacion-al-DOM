8524a29c5c18d9bfbb83430c772af271285afd52
Trex1023 <121815633+Trex1023@users.noreply.github.com>
2026-08-19 19:46:49 +0000

    6

---
+++ 6/script.js
@@ -0,0 +1,17 @@
+const lista = document.createElement('ol');
+const text1 = document.createElement('li')
+text1.textContent = "1"
+const text2 = document.createElement('li')
+text2.textContent = "2"
+const text3 = document.createElement('li')
+text3.textContent = "3"
+const text4 = document.createElement('li')
+text4.textContent = "4"
+const text5 = document.createElement('li')
+text5.textContent = "5"
+lista.appendChild(text1)
+lista.appendChild(text2)
+lista.appendChild(text3)
+lista.appendChild(text4)
+lista.appendChild(text5)
+document.body.appendChild(lista);
